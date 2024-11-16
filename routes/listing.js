d60448ceb9a314efdcb548dec21ba356abfa5cf3
khushal41 <114830517+Khushal41@users.noreply.github.com>
2024-11-16 16:51:49 +0530

    Commit

--- routes/listing.js
+++ routes/listing.js
@@ -39,6 +39,7 @@ router.post("/", validateListing,
         console.log(req.body); // Log the form data
         const newListing = new Listing(req.body.listing);
         await newListing.save();
+        req.flash("success", "New Listing Created");
         res.redirect("/listings");
     }));
 
--- schema.js
+++ schema.js
@@ -1,31 +1,9 @@
-// const Joi = require('joi');
-
-// module.exports.listingSchema = Joi.object({
-//     listing: Joi.object({
-//         title: Joi.string().required(),
-//         description: Joi.string().required(),
-//         image: Joi.string().allow("", null),
-//         price: Joi.number().required().min(0),
-//         country: Joi.string().required(),
-//         location: Joi.string().required(),
-//     }).required()
-// });
-
-
-// module.exports.reviewSchema = Joi.object({
-//     review: Joi.object({
-//         rating: Joi.number().required().min(1).max(5),
-//         comment: Joi.string().required(),
-//     }).required()
-// });
-
-
 const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
-        title: Joi.string().required().min(3).max(100),
-        description: Joi.string().required().min(10).max(1000),
+        title: Joi.string().required().min(1).max(100),
+        description: Joi.string().required().min(1).max(1000),
         image: Joi.string().uri().allow("", null),
         price: Joi.number().required().min(0),
         country: Joi.string().pattern(/^[a-zA-Z\s]+$/).required(),
