9e207e54bbbd50ef7cccb25b4d8b57739e2a9a65
khushal41 <114830517+Khushal41@users.noreply.github.com>
2024-11-17 18:19:42 +0530

    Commit

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlustDataBase";
 
@@ -54,6 +55,15 @@ app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User({
+//         email: "student@gmail.com",
+//         username: "delta-student",
+//     });
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+// })
+
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -70,8 +80,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page not Found!"));
